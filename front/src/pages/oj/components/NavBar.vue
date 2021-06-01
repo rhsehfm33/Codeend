ee5860e1fc31759778bcb596222470af0f1b1858
ShinYeseul <kymsdh1219@hanmail.net>
2021-06-01 17:41:10 +0900

    [feat,frontend,3,13]: fix register and login + ko.js

--- front/src/pages/oj/components/NavBar.vue
+++ front/src/pages/oj/components/NavBar.vue
@@ -71,12 +71,6 @@
                   shape="circle"
                   @click="handleBtnClick('login')">{{$t('m.Login')}}
           </Button>
-          <Button v-if="website.allow_register"
-                  type="ghost"
-                  shape="circle"
-                  @click="handleBtnClick('register')"
-                  style="color: white; background-color: #2db7f5;">{{$t('m.Register')}}
-          </Button>
         </div>
       </template>  
       <template v-else class="display">

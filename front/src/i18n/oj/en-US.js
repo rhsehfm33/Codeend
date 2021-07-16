40022ecb2021e4f3818ccc595cc094a61e2f1337
izzzzzzi <noeymaiih@gmail.com>
2021-07-16 22:06:29 +0900

    [fix,frontend,22]: fix translate file

--- front/src/i18n/oj/en-US.js
+++ front/src/i18n/oj/en-US.js
@@ -33,17 +33,6 @@ export const m = {
   You_have_submitted_a_solution: 'You have submitted a solution.',
   Contest_has_ended: 'Contest has ended',
   You_have_submission_in_this_problem_sure_to_cover_it: 'You have submission in this problem, sure to cover it?',
-  // About.vue
-  Compiler: 'Compiler',
-  Result_Explanation: 'Result Explanation',
-  Pending_Judging_Description: 'You solution will be judged soon, please wait for result.',
-  Compile_Error_Description: "Failed to compile your source code. Click on the link to see compiler's output.",
-  Accepted_Description: 'Congratulations. Your solution is correct.',
-  Wrong_Answer_Description: "Your program's output doesn't match judger's answer.",
-  Runtime_Error_Description: 'Your program terminated abnormally. Possible reasons are: segment fault, divided by zero or exited with code other than 0.',
-  Time_Limit_Exceeded_Description: 'The CPU time your program used has exceeded limit.',
-  Memory_Limit_Exceeded_Description: 'The memory your program actually used has exceeded limit.',
-  System_Error_Description: 'Oops, something has gone wrong with the judger. Please report this to administrator.',
   // ACMContestRank.vue
   Menu: 'Menu',
   Chart: 'Chart',
@@ -71,43 +60,10 @@ export const m = {
   // Announcements.vue
   Contest_Announcements: 'Contest Announcements',
   By: 'By',
-
   // ApplyResetPassword.vue
   The_email_doesnt_exist: 'The email doesn\'t exist',
   Success: 'Success',
   Password_reset_mail_sent: 'Password reset mail has been sent to your email，',
-
-  // FAQ.vue
-  Frequently_Asked_Questions: 'Frequently Asked Questions',
-  Where_is_the_input_and_the_output: 'Where is the input and the output?',
-  Where_is_the_input_and_the_output_answer_part_1: 'Your program shall read input from',
-  Standard_Input: 'Standard Input',
-  Where_is_the_input_and_the_output_answer_part_3: 'and write output to',
-  Standard_Output: 'Standard Output',
-  Where_is_the_input_and_the_output_answer_part_5: 'For example,you can use',
-  Where_is_the_input_and_the_output_answer_part_6: 'in C or',
-  Where_is_the_input_and_the_output_answer_part_7: 'in C++ to read from stdin,and use',
-  Where_is_the_input_and_the_output_answer_part_8: 'in C or',
-  Where_is_the_input_and_the_output_answer_part_9: 'in C++ to write to stdout.  User programs are not allowed to read or write files, or you will get a',
-  What_is_the_meaning_of_submission_execution_time: 'What\'s the meaning of the submission execution time?',
-  What_is_the_meaning_of_submission_execution_time_answer: 'The onlinejudge might test your code multiple times with different input files. If your code gives the correct answer within the time limit for each input file, the execution time displayed is the max of the time spent for each test case. Otherwise, the execution time will have no sense.',
-  How_Can_I_use_CPP_Int64: 'How can I use C++ Int64?',
-  How_Can_I_use_CPP_Int64_answer_part_1: 'You should declare as',
-  How_Can_I_use_CPP_Int64_answer_part_2: 'and use with',
-  or: 'or',
-  using: 'using',
-  How_Can_I_use_CPP_Int64_answer_part_3: 'will result in',
-  Java_specifications: 'Java specifications?',
-  Java_specifications_answer_part_1: 'All programs must begin in a static main method in a',
-  Java_specifications_answer_part_2: 'class. Do not use public classes: even',
-  Java_specifications_answer_part_3: 'must be non public to avoid compile error.Use buffered I/O to avoid time limit exceeded due to excesive flushing.',
-  About_presentation_error: 'About presentation error?',
-  About_presentation_error_answer_part_1: 'There is no presentation error in this oj.The judger will trim the blacks and wraps in your ouput\'s',
-  last: 'last',
-  About_presentation_error_answer_part_2: 'line.  if it\'s still different with the correct output, the result will be',
-  How_to_report_bugs: 'How to report bugs about this oj?',
-  How_to_report_bugs_answer_part_1: 'The onlinejudge is open source, you can open an issue in',
-  How_to_report_bugs_answer_part_2: 'The details(like env, version..) about a bug is required, which will help us a lot to solve the bug. Certainly, we are very pleased to merge your pull requests.',
   // Cancel.vue
   Cancel: 'Cancel',
   // ContestDetail.vue
@@ -149,16 +105,11 @@ export const m = {
   Home: 'Home',
   NavProblems: 'Problems',
   Contests: 'Contests',
+  Community: 'Community',
   NavStatus: 'Status',
   Rank: 'Rank',
   ACM_Rank: 'ACM Rank',
   OI_Rank: 'OI Rank',
-  Community: 'Community',
-  About: 'About',
-  Judger: 'Judger',
-  FAQ: 'FAQ',
-  Recruit: 'Recruit',
-  Lecture: 'Lecture',
   Login: 'Login',
   Register: 'Register',
   MyHome: 'Home',
@@ -185,8 +136,9 @@ export const m = {
   // SecuritySettig
   Sessions: 'Sessions',
   Two_Factor_Authentication: 'Two Factor Authentication',
-
   // Login.vue
+  LoginUser: 'Username',
+  LoginPwd: 'Password',
   LoginUsername: 'LoginUsername',
   LoginPassword: 'LoginPassword',
   TFA_Code: 'Code from your TFA app',
@@ -194,7 +146,6 @@ export const m = {
   Forget_Password: 'Forget Password',
   UserLogin: 'UserLogin',
   Welcome_back: 'Welcome back to Codeend',
-
   // OIRank.vue
   OI_Ranklist: 'OI Ranklist',
   // OIContestRank.vue
@@ -210,7 +161,6 @@ export const m = {
   Difficulty: 'Difficulty',
   Total: 'Total',
   AC_Rate: 'AC Rate',
-
   // Register.vue
   Home_Start: 'Go to Start',
   RegisterUsername: 'Username',
@@ -224,15 +174,12 @@ export const m = {
   The_email_already_exists: 'The email already exists',
   password_does_not_match: 'password does not match',
   Thanks_for_registering: 'Thanks for your registering, you can login now',
-
   // ResetPassword.vue and ApplyResetPassword.vue
   Reset_Password: 'Reset Password',
-  RPassword: 'Password',
-  RPassword_Again: 'Password Again',
+  RCaptcha: 'Verification code',
   ApplyEmail: 'ApplyEmail',
   Send_Password_Reset_Email: 'Send Password Reset Email기',
   Your_password_has_been_reset: 'Your password has been reset.',
-
   // Save.vue
   Save: 'Save',
   // Simditor.vue
